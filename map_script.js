8f932f5a11abfcced99314b80b2ee88eeaf5ea5a
tirthapriya12 <tirthapriya.banerjee@gmail.com>
2017-04-24 00:15:47 +0530

    updated with search lists and directions on click

--- map_script.js
+++ map_script.js
@@ -1,7 +1,7 @@
 
 var source={ lat: 0, lng: 0 },
-map, markers=[] , geocoder , service,places ;
-
+map, markers=[] , geocoder , service,places=[] ,listeners=[] , directionsDisplayerarray=[] ;
+var infowindow ;
 //window.addEventListener('load',getLocation);
 
 var autocomplete;
@@ -9,7 +9,7 @@ var autocomplete;
 $(window).blur(function() {
     //do something
 
-     $(window).trigger('load');
+     //$(window).trigger('load');
 
 });
 
@@ -17,24 +17,30 @@ $(window).blur(function() {
 function initMap()
 {
 
+
 	google.maps.event.addDomListener(window, 'load', function(){
 
 		attachAutocomplete();
 
 		document.getElementById('submit').addEventListener('click',initialiseSubmit);
 
-		
+		removeListeners(); //just to be sure removing all event listeners 
+
+		getLocation();
 
 	} );
 	
-	getLocation();
+
+	removeListeners();
+
+		infowindow = new google.maps.InfoWindow();
 
 	geocoder = new google.maps.Geocoder();
 
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: source,
 		scrollwheel: false,
-		zoom: 12,
+		zoom: 11,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 
@@ -43,6 +49,7 @@ function initMap()
 	marker = new google.maps.Marker({
 		map: map,
 		position: source,
+		title:'Your Location  ',
 		icon: 'marker.png',
 		map_icon_label: '<span class="map-icon map-icon-point-of-interest">your location </span>'
 	});
@@ -70,7 +77,7 @@ function showPosition(position) {
 }
 
 function errorCallBack(msg){ 
-	alert('Please enable your GPS position future.');
+	alert('Please enable your GPS position it is required.');
 
 }
 
@@ -78,6 +85,7 @@ function errorCallBack(msg){
 function attachAutocomplete() {
 
 	autocomplete = new google.maps.places.Autocomplete(document.getElementById('alterLocation'));
+
 	autocomplete.bindTo('bounds', map);
 }
 
@@ -128,7 +136,7 @@ function attachAutocomplete() {
 
         		var request = {
     								location: source,
-   									 radius: '10000',
+   									 radius: '30000',
    									 query: "children's hospital"
  									};
 
@@ -145,9 +153,18 @@ function attachAutocomplete() {
 
         		if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < result.length; i++) {
-      var place = result[i];
-     
+
+    	if(result[i].formatted_address.indexOf('India') > -1)
+    	{
+    		 
+    		 places.push(result[i]); //using the result in an array
+
+     console.log(result[i]);
       createMarker(result[i]);
+
+    	}
+
+      
     }
   }
    else
@@ -165,11 +182,148 @@ function attachAutocomplete() {
 
         		 marker = new google.maps.Marker({
 				map: map,
+				title: result.name,
 				position: result.geometry.location ,
 				icon: 'baby-icon.png',
 			
 			});
 
+       attachListner(marker,result) 		 
 		markers.push(marker);
 
         }
+
+        function attachListner(marker,result){
+
+
+        		document.getElementById('suggestion').innerHTML+= '<div style=" display:inline-block; width:200px; word-wrap : break-word; "><strong> ' + result.name + '</strong><br><br>' +
+                'Rating: '+result.rating+" stars" + '<br><br>' +
+                result.formatted_address + '</div>';
+
+
+
+        	 var listener = google.maps.event.addListener(marker, 'mouseover', function() {
+              infowindow.setContent('<div style=" width:200px; word-wrap : break-word; "><strong> ' + result.name + '</strong><br><br>' +
+                'Rating: '+result.rating+" stars" + '<br><br>' +
+                result.formatted_address + '</div>');
+              infowindow.open(map, this);
+
+              
+
+
+            });
+
+        	
+        	 listeners.push(listener); // collecting all listeners
+
+        	listener = google.maps.event.addListener(marker, 'mouseout', function() {
+              
+              infowindow.close(map, this);
+            });
+			
+			listeners.push(listener); 
+
+
+			listener = google.maps.event.addListener(marker, 'click', function() {
+              
+              	document.getElementById('dvPanel').innerHTML='';
+
+              	removePrevDirection();
+
+              	directionShow(result);
+
+            });
+
+        }
+
+
+        function removeListeners(){
+
+
+        	  for(var j=0 ; j<listeners.length; j++)
+        	  {
+
+        	  		google.maps.event.removeListener(listeners[j]);
+        	  		
+        	  		console.log(listeners[j]);
+        	  }
+        }
+
+        function removePrevDirection()
+        {
+
+        	for (var j=0;j<  directionsDisplayerarray.length ; j++)
+        	{
+        		 directionsDisplayerarray[j].setMap(null);
+
+        	}
+
+
+        }
+
+
+         function directionShow(result)
+         {
+
+
+         		var destination1=result.geometry.location;
+
+         		var directionsDisplay = new google.maps.DirectionsRenderer({
+        map: map,
+        draggable:true
+      });
+
+       directionsDisplay.setPanel(document.getElementById('dvPanel'));
+
+        //directionsDisplayer.push(directionsDisplay);
+      // Set destination, origin and travel mode.
+      var request = {
+        destination: destination1,
+        origin: source,
+        travelMode: 'DRIVING'
+      };
+
+      
+      // Pass the directions request to the directions service.
+      var directionsService = new google.maps.DirectionsService();
+
+      directionsService.route(request, function (response, status) {
+
+          
+        if (status == 'OK') {
+          // Display the route on the map.
+          
+          directionsDisplay.setDirections(response);
+
+          directionsDisplayerarray.push(directionsDisplay); //keeping list of all directions set
+
+          var dist_service = new google.maps.DistanceMatrixService();
+
+    dist_service.getDistanceMatrix({
+        origins: [source],
+        destinations: [destination],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+    }, function (response, status) {
+        if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+            var distance = response.rows[0].elements[0].distance.text;
+            var duration = response.rows[0].elements[0].duration.text;
+            
+             // var dist_div = document.getElementById('distance');
+
+             // dist_div.innerHTML="Distance :"+distance +"<br>Duration:"+duration;
+ 
+        } else {
+            alert("Unable to find the distance via road.");
+        }
+    });
+
+
+
+
+        }
+      });
+
+         }
